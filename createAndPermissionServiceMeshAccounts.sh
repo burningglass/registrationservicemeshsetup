5afe98cbcdbd7b471172eda1a9a4b08e2684d6f8
Burning Glass <carlgmanw@gmail.com>
2022-11-27 11:28:28 +0000

    Local projects have own scripts to set up own secrets

--- createAndPermissionServiceMeshAccounts.sh
+++ createAndPermissionServiceMeshAccounts.sh
@@ -21,20 +21,16 @@ if [ "$IAM_SA_EXISTS" = "0" ]; then
   #
   gcloud iam service-accounts create $IAM_SA_NAME --description="Registration service mesh IAM service account" --display-name="${IAM_SA_NAME}"
 
-  # Step 2: Bind Google Secrets Manager access read policy to this service account
-  #
-  gcloud secrets add-iam-policy-binding registration-db-username --project $PROJECT_ID --member="serviceAccount:${IAM_SA_NAME}@${PROJECT_ID}.iam.gserviceaccount.com" --role="roles/secretmanager.secretAccessor"
-
-  # Step 3. Create the Kubernetes (GCloud GKE) service account
+  # Step 2. Create the Kubernetes (GCloud GKE) service account
   kubectl create sa --namespace $K8S_NAMESPACE_NAME $K8S_SA_NAME
 
-  # Step 4. Enable the K8S service account to impersonate the GCloud IAM service account
+  # Step 3. Enable the K8S service account to impersonate the GCloud IAM service account
   gcloud iam service-accounts add-iam-policy-binding \
      ${IAM_SA_NAME}@${PROJECT_ID}.iam.gserviceaccount.com \
      --role roles/iam.workloadIdentityUser \
      --member "serviceAccount:${PROJECT_ID}.svc.id.goog[${K8S_NAMESPACE_NAME}/${K8S_SA_NAME}]"
 
-  # Step 5. Annotate the K8S service account
+  # Step 4. Annotate the K8S service account
   kubectl annotate serviceaccount \
      --namespace $K8S_NAMESPACE_NAME $K8S_SA_NAME \
      iam.gke.io/gcp-service-account=${IAM_SA_NAME}@${PROJECT_ID}.iam.gserviceaccount.com
